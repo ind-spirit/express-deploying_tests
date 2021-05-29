eac0c48fa82c483d2140b0963bef35a9e4ba9eb8
ind-spirit <error156@yandex.ru>
2021-05-29 22:55:26 +0700

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 const path = require('path');
 
 
@@ -9,7 +9,7 @@ app.use(express.static('./assets/'));
 
 app.get('/', (req, res) => {
   //res.send('get fucked lol')
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, './index.html'));
 })
 
 app.listen(port, () => {
